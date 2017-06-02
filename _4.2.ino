d20019396e29a899e309b938f55348cf628841a5
Ciro Camey <ciroed8@gmail.com>
2017-06-01 23:48:02 -0500

    File from computer

---
+++ _4.2.ino
@@ -0,0 +1,37 @@
+
+char chrValue = 65;
+byte byteValue = 65;
+int intValue = 65;
+float floatValue = 65.0;
+
+void setup()
+{
+  Serial.begin(9600);
+}
+void loop()
+{
+  Serial.println("chrValue: ");
+  Serial.println(chrValue);
+  Serial.write(chrValue);
+  Serial.println();
+  Serial.println(chrValue,DEC);
+  Serial.println("byteValue: ");
+  Serial.println(byteValue);
+  Serial.write(byteValue);
+  Serial.println();
+  Serial.println(byteValue,DEC);
+  Serial.println("intValue: ");
+  Serial.println(intValue);
+  Serial.println(intValue,DEC);
+  Serial.println(intValue,HEX);
+  Serial.println(intValue,OCT);
+  Serial.println(intValue,BIN);
+  Serial.println("floatValue: ");
+  Serial.println(floatValue);
+
+  delay(1000); // delay a second between numbers
+  chrValue++; // to the next value
+  byteValue++;
+  intValue++;
+  floatValue +=1;
+}
